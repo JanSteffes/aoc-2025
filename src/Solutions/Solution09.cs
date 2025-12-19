d00562cd90b3494b601c31fe4c55659bbee5d717
Redensarten <jan.steffes@gmx.de>
2025-12-20 00:28:04 +0100

    Not working for 9A Input, but for Test

--- src/Solutions/Solution09.cs
+++ src/Solutions/Solution09.cs
@@ -1,5 +1,6 @@
-using System;
 using aoc_2025.Interfaces;
+using aoc_2025.SolutionUtils;
+using System.Drawing;
 
 namespace aoc_2025.Solutions
 {
@@ -7,12 +8,45 @@ namespace aoc_2025.Solutions
     {
         public string RunPartA(string inputData)
         {
-            throw new NotImplementedException();
+            var input = ParseUtils.ParseIntoLines(inputData).ToArray();
+
+            var positionsOfRedTiles = input.Select(i => i.Split(",")).Select(p => new Point(int.Parse(p[0]), int.Parse(p[1]))).ToList();
+
+            var rectangleSizes = CalculateSizes(positionsOfRedTiles);
+            var orderedRectangles = rectangleSizes.OrderByDescending(d => d.Size).ToArray();
+            var biggestRectangle = orderedRectangles.First();
+
+            return biggestRectangle.Size.ToString();
         }
 
         public string RunPartB(string inputData)
         {
             throw new NotImplementedException();
+        }
+
+        private List<(long Size, Point FirstPoint, Point SecondPoint)> CalculateSizes(List<Point> positionsOfRedTiles)
+        {
+            var result = new List<(long Size, Point FirstPoint, Point SecondPoint)>();
+            foreach (var position in positionsOfRedTiles)
+            {
+                foreach (var otherPosition in positionsOfRedTiles)
+                {
+                    if (position == otherPosition)
+                    {
+                        continue;
+                    }
+                    var areaSize = CalculateArea(position, otherPosition);
+                    result.Add((areaSize, position, otherPosition));
+                }
+            }
+            return result;
+        }
+
+        private long CalculateArea(Point position, Point otherPosition)
+        {
+            var xLength = Math.Max(position.X, otherPosition.X) - Math.Min(position.X, otherPosition.X) + 1;
+            var yLength = Math.Max(position.Y, otherPosition.Y) - Math.Min(position.Y, otherPosition.Y) + 1;
+            return xLength * yLength;
         }
     }
 }
